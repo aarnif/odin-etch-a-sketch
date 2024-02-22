6771abd4d5d1163042cfc90ce554d4aeaee0b716
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-02-22 18:04:46 +0200

    Add clear-, border-, color- and size-options

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const clearButton = document.getElementById("clear-button");
+const gridButton = document.getElementById("grid-button");
+const colorPicker = document.getElementById("color-picker");
+const gridSizeInput = document.getElementById("grid-size");
+
 // Mouse over functionality taken from https://github.com/michalosman/etch-a-sketch/blob/master/script.js
 let mouseDown = false;
 document.body.onmousedown = () => (mouseDown = true);
@@ -12,6 +17,7 @@ const createGrid = (sideLength) => {
     for (let j = 0; j < sideLength; ++j) {
       const gridRowItem = document.createElement("div");
       gridRowItem.classList.add("grid-item");
+      gridRowItem.classList.add("grid-item--border");
       gridRowItem.style.backgroundColor = "#d4d4d8";
 
       // Create an object to store the rgb values of the color, start value is black
@@ -42,6 +48,70 @@ const addColorShadeToGridItem = (gridRowItem, rgbColor) => {
     }
   });
 };
+
+const clearGrid = () => {
+  console.log("Clearing grid");
+  const gridItems = document.querySelectorAll(".grid-item");
+  gridItems.forEach((gridItem) => (gridItem.style.backgroundColor = "#d4d4d8"));
+};
+
+const addGridToScreen = () => {
+  console.log("Toggle grid to screen");
+  const gridItems = document.querySelectorAll(".grid-item");
+  gridItems.forEach((item) => {
+    item.classList.toggle("grid-item--border");
+  });
+};
+
+// Function to convert hex to rgb taken from https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
+const hexToRgb = (hex) => {
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result
+    ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16),
+      }
+    : null;
+};
+
+const changeColor = (newColor) => {
+  console.log("Changing color");
+  const gridItems = document.querySelectorAll(".grid-item");
+  gridItems.forEach((item) => {
+    addColorShadeToGridItem(item, newColor);
+  });
+};
+
+const changeGridSize = (newSize) => {
+  console.log("Changing grid size");
+  const gridContainer = document.getElementById("grid-container");
+  gridContainer.innerHTML = "";
+  createGrid(newSize);
+};
+
+clearButton.addEventListener("click", clearGrid);
+
+gridButton.addEventListener("click", addGridToScreen);
+
+colorPicker.addEventListener("change", (e) => {
+  const newColor = e.target.value;
+  console.log(`New color: ${newColor}`);
+  const convertColorToRgb = hexToRgb(newColor);
+  changeColor(convertColorToRgb);
+});
+
+gridSizeInput.addEventListener("change", (e) => {
+  const newSize = e.target.value;
+  console.log(`New grid size: ${newSize}`);
+  changeGridSize(newSize);
+});
+
+gridSizeInput.addEventListener("mousemove", (e) => {
+  const currentGridSize = document.getElementById("current-size");
+  const newSize = gridSizeInput.value;
+  currentGridSize.textContent = `${newSize}x${newSize}`;
+});
 
 window.onload = () => {
   createGrid(50);
